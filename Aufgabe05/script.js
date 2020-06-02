401de69964520fd447df1be262f018d528d598e5
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-06-02 16:24:42 +0200

    Aufgabe 5: update3

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -4,95 +4,110 @@ let new01 = {
     name: "Graue rundliche Katze",
     beschreibung: "Die Augen glänzen etwas",
     preis: 4.99,
-    bild: "figurA.png"
+    bild: "figurA.png",
+    kategorie: "neuheiten"
 };
 // Angebote
 let sale01 = {
     name: "unfertige Katzen-Figur",
     beschreibung: "(verflucht!)",
     preis: 6.66,
-    bild: "figurB.png"
+    bild: "figurB.png",
+    kategorie: "angebote"
 };
 let sale02 = {
     name: "unfertige Katzen-Figur 2",
     beschreibung: "Eine schlafende Katze (nicht verflucht)",
     preis: 2.69,
-    bild: "figurC.png"
+    bild: "figurC.png",
+    kategorie: "angebote"
 };
 // Figuren
 let figur01 = {
     name: "Schleich Katze laufend",
     beschreibung: "Eine laufende Katze",
     preis: 11.99,
-    bild: "figur1.png"
+    bild: "figur1.png",
+    kategorie: "figuren"
 };
 let figur02 = {
     name: "Schleich Katze stehend",
     beschreibung: "Eine stehende Katze",
     preis: 9.99,
-    bild: "figur2.png"
+    bild: "figur2.png",
+    kategorie: "figuren"
 };
 let figur03 = {
     name: "Schleich Katze sitzend",
     beschreibung: "Eine sitzende Katze",
     preis: 9.99,
-    bild: "figur3.png"
+    bild: "figur3.png",
+    kategorie: "figuren"
 };
 let figur04 = {
     name: "Schleich Katze putzend",
     beschreibung: "Eine sich putzende Katze",
     preis: 6.99,
-    bild: "figur4.png"
+    bild: "figur4.png",
+    kategorie: "figuren"
 };
 let figur05 = {
     name: "Schleich Katze streckend",
     beschreibung: "Eine sich streckende Katze",
     preis: 6.99,
-    bild: "figur5.png"
+    bild: "figur5.png",
+    kategorie: "figuren"
 };
 let figur06 = {
     name: "Schleich spielende Kätzchen",
     beschreibung: "Kätzchen, die spielen",
     preis: 8.99,
-    bild: "figur6.png"
+    bild: "figur6.png",
+    kategorie: "figuren"
 };
 let figur07 = {
     name: "Schleich spielende Kätzchen 2",
     beschreibung: "schon wieder",
     preis: 8.99,
-    bild: "figur7.png"
+    bild: "figur7.png",
+    kategorie: "figuren"
 };
 let figur08 = {
     name: "Schleich Maine-Coon-Katze",
     beschreibung: "Eine stehende Langhaarkatze",
     preis: 3.99,
-    bild: "figur8.png"
+    bild: "figur8.png",
+    kategorie: "figuren"
 };
 let figur09 = {
     name: "Schleich schauende Katze",
     beschreibung: "[originelle Beschreibung]",
     preis: 3.99,
-    bild: "figur9.png"
+    bild: "figur9.png",
+    kategorie: "figuren"
 };
 // Plüschtiere
 let pluesch01 = {
     name: "Plüsch-Katzen-Anhänger",
     beschreibung: "Mit Reißverschluss!",
     preis: 4.99,
-    bild: "pluesch1.png"
+    bild: "pluesch1.png",
+    kategorie: "plueschtiere"
 };
 let pluesch02 = {
     name: "FurReal friends Samtpfötchen",
     beschreibung: "Schläft schon seit Jahren. Funktioniert noch!",
     preis: 99.99,
-    bild: "pluesch2.png"
+    bild: "pluesch2.png",
+    kategorie: "plueschtiere"
 };
 // Sonstiges
 let sonstiges01 = {
     name: "Katzen-Kerze",
     beschreibung: "Echte Bienenwachs-Kerze in Katzenform",
     preis: 14.99,
-    bild: "kerze.png"
+    bild: "kerze.png",
+    kategorie: "sonstiges"
 };
 //#endregion
 // Arrays nach Kategorie
@@ -101,154 +116,41 @@ const saleArtikel = [sale01, sale02];
 const figurArtikel = [figur01, figur02, figur03, figur04, figur05, figur06, figur07, figur08, figur09];
 const plueschArtikel = [pluesch01, pluesch02];
 const sonstigesArtikel = [sonstiges01];
-for (let i = 0; i < newArtikel.length; i++) {
-    //div generieren
-    let newDiv = document.createElement("div");
-    newDiv.id = "neuheitNr" + i;
-    document.getElementById("neuheiten")?.appendChild(newDiv);
-    //img
-    let bildImg = document.createElement("img");
-    bildImg.src = newArtikel[i].bild;
-    document.getElementById("neuheitNr" + i)?.appendChild(bildImg);
-    //name
-    let nameP = document.createElement("p");
-    nameP.innerHTML = newArtikel[i].name;
-    document.getElementById("neuheitNr" + i)?.appendChild(nameP);
-    //beschreibung
-    let beschreibungP = document.createElement("p");
-    beschreibungP.innerHTML = newArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("neuheitNr" + i)?.appendChild(beschreibungP);
-    //preis
-    let preisP = document.createElement("p");
-    preisP.innerHTML = newArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("neuheitNr" + i)?.appendChild(preisP);
-    //input
-    let kaufen = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("neuheitNr" + i)?.appendChild(kaufen);
-}
-for (let i = 0; i < saleArtikel.length; i++) {
-    //div generieren
-    let newDiv = document.createElement("div");
-    newDiv.id = "angebotNr" + i;
-    document.getElementById("angebote")?.appendChild(newDiv);
-    //img
-    let bildImg = document.createElement("img");
-    bildImg.src = saleArtikel[i].bild;
-    document.getElementById("angebotNr" + i)?.appendChild(bildImg);
-    //name
-    let nameP = document.createElement("p");
-    nameP.innerHTML = saleArtikel[i].name;
-    document.getElementById("angebotNr" + i)?.appendChild(nameP);
-    //beschreibung
-    let beschreibungP = document.createElement("p");
-    beschreibungP.innerHTML = saleArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("angebotNr" + i)?.appendChild(beschreibungP);
-    //preis
-    let preisP = document.createElement("p");
-    preisP.innerHTML = saleArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "reduziert";
-    document.getElementById("angebotNr" + i)?.appendChild(preisP);
-    //input
-    let kaufen = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("angebotNr" + i)?.appendChild(kaufen);
-}
-for (let i = 0; i < figurArtikel.length; i++) {
-    //div generieren
-    let newDiv = document.createElement("div");
-    newDiv.id = "figurNr" + i;
-    document.getElementById("figuren")?.appendChild(newDiv);
-    //img
-    let bildImg = document.createElement("img");
-    bildImg.src = figurArtikel[i].bild;
-    document.getElementById("figurNr" + i)?.appendChild(bildImg);
-    //name
-    let nameP = document.createElement("p");
-    nameP.innerHTML = figurArtikel[i].name;
-    document.getElementById("figurNr" + i)?.appendChild(nameP);
-    //beschreibung
-    let beschreibungP = document.createElement("p");
-    beschreibungP.innerHTML = figurArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("figurNr" + i)?.appendChild(beschreibungP);
-    //preis
-    let preisP = document.createElement("p");
-    preisP.innerHTML = figurArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("figurNr" + i)?.appendChild(preisP);
-    //input
-    let kaufen = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("figurNr" + i)?.appendChild(kaufen);
-}
-for (let i = 0; i < plueschArtikel.length; i++) {
-    //div generieren
-    let newDiv = document.createElement("div");
-    newDiv.id = "plueschNr" + i;
-    document.getElementById("plueschtiere")?.appendChild(newDiv);
-    //img
-    let bildImg = document.createElement("img");
-    bildImg.src = plueschArtikel[i].bild;
-    document.getElementById("plueschNr" + i)?.appendChild(bildImg);
-    //name
-    let nameP = document.createElement("p");
-    nameP.innerHTML = plueschArtikel[i].name;
-    document.getElementById("plueschNr" + i)?.appendChild(nameP);
-    //beschreibung
-    let beschreibungP = document.createElement("p");
-    beschreibungP.innerHTML = plueschArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("plueschNr" + i)?.appendChild(beschreibungP);
-    //preis
-    let preisP = document.createElement("p");
-    preisP.innerHTML = plueschArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("plueschNr" + i)?.appendChild(preisP);
-    //input
-    let kaufen = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("plueschNr" + i)?.appendChild(kaufen);
-}
-for (let i = 0; i < sonstigesArtikel.length; i++) {
-    //div generieren
-    let newDiv = document.createElement("div");
-    newDiv.id = "sonstigesNr" + i;
-    document.getElementById("sonstiges")?.appendChild(newDiv);
-    //img
-    let bildImg = document.createElement("img");
-    bildImg.src = sonstigesArtikel[i].bild;
-    document.getElementById("sonstigesNr" + i)?.appendChild(bildImg);
-    //name
-    let nameP = document.createElement("p");
-    nameP.innerHTML = sonstigesArtikel[i].name;
-    document.getElementById("sonstigesNr" + i)?.appendChild(nameP);
-    //beschreibung
-    let beschreibungP = document.createElement("p");
-    beschreibungP.innerHTML = sonstigesArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("sonstigesNr" + i)?.appendChild(beschreibungP);
-    //preis
-    let preisP = document.createElement("p");
-    preisP.innerHTML = sonstigesArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("sonstigesNr" + i)?.appendChild(preisP);
-    //input
-    let kaufen = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("sonstigesNr" + i)?.appendChild(kaufen);
+artikelErstellen(newArtikel);
+artikelErstellen(saleArtikel);
+artikelErstellen(figurArtikel);
+artikelErstellen(plueschArtikel);
+artikelErstellen(sonstigesArtikel);
+function artikelErstellen(artikelArray) {
+    for (let i = 0; i < artikelArray.length; i++) {
+        //div generieren
+        let newDiv = document.createElement("div");
+        newDiv.id = artikelArray[i].kategorie + i;
+        document.getElementById(artikelArray[i].kategorie)?.appendChild(newDiv);
+        //img
+        let bildImg = document.createElement("img");
+        bildImg.src = artikelArray[i].bild;
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(bildImg);
+        //name
+        let nameP = document.createElement("p");
+        nameP.innerHTML = artikelArray[i].name;
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(nameP);
+        //beschreibung
+        let beschreibungP = document.createElement("p");
+        beschreibungP.innerHTML = artikelArray[i].beschreibung;
+        beschreibungP.className = "beschreibung";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(beschreibungP);
+        //preis
+        let preisP = document.createElement("p");
+        preisP.innerHTML = artikelArray[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+        preisP.className = "preis";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(preisP);
+        //input
+        let kaufen = document.createElement("input");
+        kaufen.type = "image";
+        kaufen.src = "4_bild_einkaufstasche_klein.png";
+        kaufen.alt = "kaufen";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(kaufen);
+    }
 }
 //# sourceMappingURL=script.js.map
--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -4,6 +4,7 @@ interface Artikel {
     beschreibung: string;
     preis: number;
     bild: string;
+    kategorie: string;
 }
 
 // Neuheiten
@@ -11,7 +12,8 @@ let new01: Artikel = {
     name: "Graue rundliche Katze",
     beschreibung: "Die Augen glänzen etwas",
     preis: 4.99,
-    bild: "figurA.png"
+    bild: "figurA.png",
+    kategorie: "neuheiten"
 };
 
 // Angebote
@@ -19,14 +21,16 @@ let sale01: Artikel = {
     name: "unfertige Katzen-Figur",
     beschreibung: "(verflucht!)",
     preis: 6.66,
-    bild: "figurB.png"
+    bild: "figurB.png",
+    kategorie: "angebote"
 };
 
 let sale02: Artikel = {
     name: "unfertige Katzen-Figur 2",
     beschreibung: "Eine schlafende Katze (nicht verflucht)",
     preis: 2.69,
-    bild: "figurC.png"
+    bild: "figurC.png",
+    kategorie: "angebote"
 };
 
 // Figuren
@@ -34,63 +38,72 @@ let figur01: Artikel = {
     name: "Schleich Katze laufend",
     beschreibung: "Eine laufende Katze",
     preis: 11.99,
-    bild: "figur1.png"
+    bild: "figur1.png",
+    kategorie: "figuren"
 };
 
 let figur02: Artikel = {
     name: "Schleich Katze stehend",
     beschreibung: "Eine stehende Katze",
     preis: 9.99,
-    bild: "figur2.png"
+    bild: "figur2.png",
+    kategorie: "figuren"
 };
 
 let figur03: Artikel = {
     name: "Schleich Katze sitzend",
     beschreibung: "Eine sitzende Katze",
     preis: 9.99,
-    bild: "figur3.png"
+    bild: "figur3.png",
+    kategorie: "figuren"
 };
 
 let figur04: Artikel = {
     name: "Schleich Katze putzend",
     beschreibung: "Eine sich putzende Katze",
     preis: 6.99,
-    bild: "figur4.png"
+    bild: "figur4.png",
+    kategorie: "figuren"
 };
 
 let figur05: Artikel = {
     name: "Schleich Katze streckend",
     beschreibung: "Eine sich streckende Katze",
     preis: 6.99,
-    bild: "figur5.png"
+    bild: "figur5.png",
+    kategorie: "figuren"
 };
 
 let figur06: Artikel = {
     name: "Schleich spielende Kätzchen",
     beschreibung: "Kätzchen, die spielen",
     preis: 8.99,
-    bild: "figur6.png"
+    bild: "figur6.png",
+    kategorie: "figuren"
 };
 
 let figur07: Artikel = {
     name: "Schleich spielende Kätzchen 2",
     beschreibung: "schon wieder",
     preis: 8.99,
-    bild: "figur7.png"
+    bild: "figur7.png",
+    kategorie: "figuren"
 };
 
 let figur08: Artikel = {
     name: "Schleich Maine-Coon-Katze",
     beschreibung: "Eine stehende Langhaarkatze",
     preis: 3.99,
-    bild: "figur8.png"
+    bild: "figur8.png",
+    kategorie: "figuren"
 };
 
 let figur09: Artikel = {
     name: "Schleich schauende Katze",
     beschreibung: "[originelle Beschreibung]",
     preis: 3.99,
-    bild: "figur9.png"
+    bild: "figur9.png",
+    kategorie: "figuren"
 };
 
 // Plüschtiere
@@ -98,14 +111,16 @@ let pluesch01: Artikel = {
     name: "Plüsch-Katzen-Anhänger",
     beschreibung: "Mit Reißverschluss!",
     preis: 4.99,
-    bild: "pluesch1.png"
+    bild: "pluesch1.png",
+    kategorie: "plueschtiere"
 };
 
 let pluesch02: Artikel = {
     name: "FurReal friends Samtpfötchen",
     beschreibung: "Schläft schon seit Jahren. Funktioniert noch!",
     preis: 99.99,
-    bild: "pluesch2.png"
+    bild: "pluesch2.png",
+    kategorie: "plueschtiere"
 };
 
 // Sonstiges
@@ -113,7 +128,8 @@ let sonstiges01: Artikel = {
     name: "Katzen-Kerze",
     beschreibung: "Echte Bienenwachs-Kerze in Katzenform",
     preis: 14.99,
-    bild: "kerze.png"
+    bild: "kerze.png",
+    kategorie: "sonstiges"
 };
 //#endregion
 
@@ -124,188 +140,47 @@ const figurArtikel: Artikel[] = [figur01, figur02, figur03, figur04, figur05, fi
 const plueschArtikel: Artikel[] = [pluesch01, pluesch02];
 const sonstigesArtikel: Artikel[] = [sonstiges01];
 
+artikelErstellen(newArtikel);
+artikelErstellen(saleArtikel);
+artikelErstellen(figurArtikel);
+artikelErstellen(plueschArtikel);
+artikelErstellen(sonstigesArtikel);
 
-for (let i: number = 0; i < newArtikel.length; i++) {
+function artikelErstellen(artikelArray: Artikel[]): void {
+    
+    for (let i: number = 0; i < artikelArray.length; i++) {
+        //div generieren
+        let newDiv: HTMLDivElement = document.createElement("div");
+        newDiv.id = artikelArray[i].kategorie + i;
+        document.getElementById(artikelArray[i].kategorie)?.appendChild(newDiv);
 
-    //div generieren
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "neuheitNr" + i;
-    document.getElementById("neuheiten")?.appendChild(newDiv);
+        //img
+        let bildImg: HTMLImageElement = document.createElement("img");
+        bildImg.src = artikelArray[i].bild;
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(bildImg);
 
-    //img
-    let bildImg: HTMLImageElement = document.createElement("img");
-    bildImg.src = newArtikel[i].bild;
-    document.getElementById("neuheitNr" + i)?.appendChild(bildImg);
+        //name
+        let nameP: HTMLParagraphElement = document.createElement("p");
+        nameP.innerHTML = artikelArray[i].name;
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(nameP);
 
-    //name
-    let nameP: HTMLParagraphElement = document.createElement("p");
-    nameP.innerHTML = newArtikel[i].name;
-    document.getElementById("neuheitNr" + i)?.appendChild(nameP);
+        //beschreibung
+        let beschreibungP: HTMLParagraphElement = document.createElement("p");
+        beschreibungP.innerHTML = artikelArray[i].beschreibung;
+        beschreibungP.className = "beschreibung";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(beschreibungP);
 
-    //beschreibung
-    let beschreibungP: HTMLParagraphElement = document.createElement("p");
-    beschreibungP.innerHTML = newArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("neuheitNr" + i)?.appendChild(beschreibungP);
+        //preis
+        let preisP: HTMLParagraphElement = document.createElement("p");
+        preisP.innerHTML = artikelArray[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+        preisP.className = "preis";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(preisP);
 
-    //preis
-    let preisP: HTMLParagraphElement = document.createElement("p");
-    preisP.innerHTML = newArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("neuheitNr" + i)?.appendChild(preisP);
-
-    //input
-    let kaufen: HTMLInputElement = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("neuheitNr" + i)?.appendChild(kaufen);
-}
-
-for (let i: number = 0; i < saleArtikel.length; i++) {
-
-    //div generieren
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "angebotNr" + i;
-    document.getElementById("angebote")?.appendChild(newDiv);
-
-    //img
-    let bildImg: HTMLImageElement = document.createElement("img");
-    bildImg.src = saleArtikel[i].bild;
-    document.getElementById("angebotNr" + i)?.appendChild(bildImg);
-
-    //name
-    let nameP: HTMLParagraphElement = document.createElement("p");
-    nameP.innerHTML = saleArtikel[i].name;
-    document.getElementById("angebotNr" + i)?.appendChild(nameP);
-
-    //beschreibung
-    let beschreibungP: HTMLParagraphElement = document.createElement("p");
-    beschreibungP.innerHTML = saleArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("angebotNr" + i)?.appendChild(beschreibungP);
-
-    //preis
-    let preisP: HTMLParagraphElement = document.createElement("p");
-    preisP.innerHTML = saleArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "reduziert";
-    document.getElementById("angebotNr" + i)?.appendChild(preisP);
-
-    //input
-    let kaufen: HTMLInputElement = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("angebotNr" + i)?.appendChild(kaufen);
-}
-
-for (let i: number = 0; i < figurArtikel.length; i++) {
-
-    //div generieren
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "figurNr" + i;
-    document.getElementById("figuren")?.appendChild(newDiv);
-
-    //img
-    let bildImg: HTMLImageElement = document.createElement("img");
-    bildImg.src = figurArtikel[i].bild;
-    document.getElementById("figurNr" + i)?.appendChild(bildImg);
-
-    //name
-    let nameP: HTMLParagraphElement = document.createElement("p");
-    nameP.innerHTML = figurArtikel[i].name;
-    document.getElementById("figurNr" + i)?.appendChild(nameP);
-
-    //beschreibung
-    let beschreibungP: HTMLParagraphElement = document.createElement("p");
-    beschreibungP.innerHTML = figurArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("figurNr" + i)?.appendChild(beschreibungP);
-
-    //preis
-    let preisP: HTMLParagraphElement = document.createElement("p");
-    preisP.innerHTML = figurArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("figurNr" + i)?.appendChild(preisP);
-
-    //input
-    let kaufen: HTMLInputElement = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("figurNr" + i)?.appendChild(kaufen);
-}
-
-for (let i: number = 0; i < plueschArtikel.length; i++) {
-
-    //div generieren
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "plueschNr" + i;
-    document.getElementById("plueschtiere")?.appendChild(newDiv);
-
-    //img
-    let bildImg: HTMLImageElement = document.createElement("img");
-    bildImg.src = plueschArtikel[i].bild;
-    document.getElementById("plueschNr" + i)?.appendChild(bildImg);
-
-    //name
-    let nameP: HTMLParagraphElement = document.createElement("p");
-    nameP.innerHTML = plueschArtikel[i].name;
-    document.getElementById("plueschNr" + i)?.appendChild(nameP);
-
-    //beschreibung
-    let beschreibungP: HTMLParagraphElement = document.createElement("p");
-    beschreibungP.innerHTML = plueschArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("plueschNr" + i)?.appendChild(beschreibungP);
-
-    //preis
-    let preisP: HTMLParagraphElement = document.createElement("p");
-    preisP.innerHTML = plueschArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("plueschNr" + i)?.appendChild(preisP);
-
-    //input
-    let kaufen: HTMLInputElement = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("plueschNr" + i)?.appendChild(kaufen);
-}
-
-for (let i: number = 0; i < sonstigesArtikel.length; i++) {
-
-    //div generieren
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "sonstigesNr" + i;
-    document.getElementById("sonstiges")?.appendChild(newDiv);
-
-    //img
-    let bildImg: HTMLImageElement = document.createElement("img");
-    bildImg.src = sonstigesArtikel[i].bild;
-    document.getElementById("sonstigesNr" + i)?.appendChild(bildImg);
-
-    //name
-    let nameP: HTMLParagraphElement = document.createElement("p");
-    nameP.innerHTML = sonstigesArtikel[i].name;
-    document.getElementById("sonstigesNr" + i)?.appendChild(nameP);
-
-    //beschreibung
-    let beschreibungP: HTMLParagraphElement = document.createElement("p");
-    beschreibungP.innerHTML = sonstigesArtikel[i].beschreibung;
-    beschreibungP.className = "beschreibung";
-    document.getElementById("sonstigesNr" + i)?.appendChild(beschreibungP);
-
-    //preis
-    let preisP: HTMLParagraphElement = document.createElement("p");
-    preisP.innerHTML = sonstigesArtikel[i].preis.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    preisP.className = "preis";
-    document.getElementById("sonstigesNr" + i)?.appendChild(preisP);
-
-    //input
-    let kaufen: HTMLInputElement = document.createElement("input");
-    kaufen.type = "image";
-    kaufen.src = "4_bild_einkaufstasche_klein.png";
-    kaufen.alt = "kaufen";
-    document.getElementById("sonstigesNr" + i)?.appendChild(kaufen);
+        //input
+        let kaufen: HTMLInputElement = document.createElement("input");
+        kaufen.type = "image";
+        kaufen.src = "4_bild_einkaufstasche_klein.png";
+        kaufen.alt = "kaufen";
+        document.getElementById(artikelArray[i].kategorie + i)?.appendChild(kaufen);
+    }
 }
