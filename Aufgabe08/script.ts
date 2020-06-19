f9f844e11869a5b5b68e9c89042fe5d6927ba033
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-06-19 15:02:33 +0200

    Aufgabe 8: update4

--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,19 +1,17 @@
 namespace Aufgabe08 {
 
-  //let url: string = "https://localhost:8100";
-  let url: string = "https://gis-sose-2020.herokuapp.com/";
   document.getElementById("send")?.addEventListener("click", handleClickSend);
 
   async function handleClickSend(): Promise<void> {
+    let formData: FormData = new FormData(document.forms[0]);
+    let url: string = "https://gis-sose-2020.herokuapp.com/";
+    let query: URLSearchParams = new URLSearchParams(<any> formData);
+    url += url + "?" + query.toString();
     serverCommunicate(url);
   }
 
   async function serverCommunicate(_url: RequestInfo): Promise<void> {
-    let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "" + _url;
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-    url += "?" + query.toString();
-    let response1: Response = await fetch(url, { method: "get" });
+    let response1: Response = await fetch(_url, { method: "get" });
     let response2: String = await response1.text();
     console.log(response2);
   }
--- Aufgabe08/serverbeispiel.ts
+++ Aufgabe08/serverbeispiel.ts
@@ -1,33 +1,33 @@
 import * as Http from "http";
 
 export namespace A08Server {
-  // Konsolenausgabe:
-  console.log("Starting server");
-  
-  let port: number = Number(process.env.PORT);
-  if (!port)
-    port = 8100; // -> localhost
 
+  console.log("Starting server");
+  // Port als Variable des Typs 'number'
+  let port: number = Number(process.env.PORT);
+  // Wenn kein Port, dann:
+  if (!port) 
+    port = 8100; // -> localhost
+  // Server als Variable des Typs "Http.Server"
   let server: Http.Server = Http.createServer();
   server.addListener("request", handleRequest);
   server.addListener("listening", handleListen);
+  // Server hört Port ab
   server.listen(port);
 
   function handleListen(): void {
-    // Konsolenausgabe:
     console.log("Listening");
   }
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log("I hear voices!");
-    // ändert Header (des HTML Dokuments):
+    // Header Attriute festlegen
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
-    // schreibt die Antwort (in HTML Dokument):
+    // schreibt & gibt "_request.url" aus:
     _response.write(_request.url);
-    // Konsolenausgabe:
     console.log(_request.url);
-    // beendet Vorgang und bringt den Elemente in den vorherigen Zustand zurück:
+    // beendet Vorgang:
     _response.end();
   }
 }
