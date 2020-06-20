65b1ecc75994ab766813047140892279014be931
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-06-20 21:28:02 +0200

    Aufgabe 8: update7

--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -2,18 +2,21 @@ namespace Aufgabe08 {
 
   document.getElementById("send")?.addEventListener("click", handleClickSend);
 
-  function handleClickSend(): void {
+  async function handleClickSend(): Promise<void> {
     let formData: FormData = new FormData(document.forms[0]);
     let url: string = "https://gis-sose-2020.herokuapp.com/";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
     url = url + "?" + query.toString();
-    communicate(url);
-  }
-
-  async function communicate(_url: RequestInfo): Promise<void> {
-    let response1: Response = await fetch(_url);
-    let response2: String = await response1.text();
+    
+    let response1: Response = await fetch(url);
+    let response2: string = response1.url;
     console.log(response2);
+
+    for (let entry of query) {
+      console.log(entry);
+      console.log("name: " + entry[0]);
+      console.log("value: " + entry[1]);
+  }
   }
 
 }
