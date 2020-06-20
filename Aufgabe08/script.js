23d1eb86aa23de5a03797650dba61b899536fb81
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-06-20 20:19:24 +0200

    Aufgabe 8: update6

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -1,17 +1,16 @@
 "use strict";
 var Aufgabe08;
 (function (Aufgabe08) {
-    document.getElementById("send").addEventListener("click", handleClickSend);
-    async function handleClickSend() {
+    document.getElementById("send")?.addEventListener("click", handleClickSend);
+    function handleClickSend() {
         let formData = new FormData(document.forms[0]);
         let url = "https://gis-sose-2020.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        await fetch(url);
         serverCommunicate(url);
     }
     async function serverCommunicate(_url) {
-        let response1 = await fetch(_url, { method: "get" });
+        let response1 = await fetch(_url);
         let response2 = await response1.text();
         console.log(response2);
     }
