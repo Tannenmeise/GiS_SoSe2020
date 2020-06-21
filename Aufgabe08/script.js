e7c829718758e0b8bc274d1955cf3ac01b199451
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-06-21 14:02:52 +0200

    Aufgabe 8: update9

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -2,19 +2,17 @@
 var Aufgabe08;
 (function (Aufgabe08) {
     document.getElementById("send")?.addEventListener("click", handleClickSend);
-    async function handleClickSend() {
+    function handleClickSend() {
         let formData = new FormData(document.forms[0]);
         let url = "https://gis-sose-2020.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        let response1 = await fetch(url);
-        let response2 = response1.url;
+        communicate(url);
+    }
+    async function communicate(_url) {
+        let response1 = await fetch(_url);
+        let response2 = await response1.text();
         console.log(response2);
-        for (let entry of query) {
-            console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
-        }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -2,21 +2,18 @@ namespace Aufgabe08 {
 
   document.getElementById("send")?.addEventListener("click", handleClickSend);
 
-  async function handleClickSend(): Promise<void> {
+  function handleClickSend(): void {
     let formData: FormData = new FormData(document.forms[0]);
     let url: string = "https://gis-sose-2020.herokuapp.com/";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
     url = url + "?" + query.toString();
+    communicate(url);
+  } 
 
-    let response1: Response = await fetch(url);
-    let response2: string = await response1.url;
+  async function communicate(_url: RequestInfo): Promise<void> {
+    let response1: Response = await fetch(_url);
+    let response2: String = await response1.text();
     console.log(response2);
-
-    for (let entry of query) {
-      console.log(entry);
-      console.log("name: " + entry[0]);
-      console.log("value: " + entry[1]);
-    }
   }
 
 }
